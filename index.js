4346711df7bf7d8bc79fdb195dc30c95ab3c9de1
Davide <80689057+Davide255@users.noreply.github.com>
2024-10-29 20:09:22 +0100

    Initial release

---
+++ index.js
@@ -0,0 +1,149 @@
+function generate_dates(data) {
+    var out = {};
+    var selected = [];
+    var left = [];
+
+    for (let day in data) {
+        if (data[day][0].length > data[day][2]) {
+            let shuffled = data[day][0].sort(() => 0.5 - Math.random());
+            out[day] = shuffled.slice(0, data[day][2]);
+        }
+        else {
+            out[day] = data[day][0];
+        }
+
+        data[day][2] -= out[day].length;
+        selected = selected.concat(out[day]);
+
+        left = left.concat(data[day][1]);
+    }
+
+    for (let day in data) {
+        if (data[day][2] > 0) {
+            let n_d = data[day][1].filter((name) => !selected.includes(name));
+            let l = 0;
+            if (n_d.length > data[day][2]) {
+                let shuffled = n_d.sort(() => 0.5 - Math.random());
+                out[day] = out[day].concat(shuffled.slice(0, data[day][2]));
+                l = data[day][2];
+            } else {
+                out[day] = out[day].concat(n_d);
+                l = n_d.length;
+            }
+
+            data[day][2] -= l;
+            selected = selected.concat(n_d);
+        }
+    }
+
+    left = left.filter((name) => !selected.includes(name));
+
+    let shuffled = left.sort(() => 0.5 - Math.random());
+
+    for (let day in data) {
+        if (data[day][2] > 0) {
+            out[day] = out[day].concat(shuffled.splice(0, data[day][2]));
+        }
+    }
+
+    return out;
+}
+
+var test_data = {
+    "1" : [["Dave","Pippo"], [], 1],
+    "2" : [["Ludo", "Pasquale"], ["Franco", "Dave"], 2],
+    "3" : [["jonny"], ["francesco", "Pasquale"], 1],
+    "4" : [["Beppe", "Franco"], ["Pasquale", "Beppe"], 2],
+    "5": [["francesco"], ["Pippo", "jonny"], 2]
+}
+
+
+function add_date() {
+    let grid = document.getElementById("date-grid");
+
+    let new_element = document.createElement("div");
+
+    new_element.className = "date";
+
+    let elements = `<div class="date-heading">
+    <input type="date" data-date="" data-date-format="DD/MM" value="2024-10-12">
+        <div>
+            <p>Numero di interrogati:</p>
+            <input type="number" value="2" min="0">
+        </div>
+    </div>
+    <ul>
+        <button onclick="add_preference(this)" class="add-preference">+</button>
+    </ul>
+    <ul>
+        <button onclick="add_preference(this)" class="add-preference">+</button>
+    </ul>
+</div>`;
+
+    new_element.innerHTML = elements;
+
+    grid.appendChild(new_element);
+}
+
+function add_preference(element) {
+    let new_element = document.createElement("input");
+    new_element.type = "text";
+    new_element.placeholder = "Name"
+    element.parentElement.insertBefore(new_element, element);
+}
+
+function openNav() {
+    document.getElementById("side-nav").style.width = "250px";
+    //document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("side-nav").style.width = "0";
+    //document.getElementById("main").style.marginLeft = "0";
+}
+
+function generate() {
+    let grid = document.getElementById("date-grid");
+
+    let data = {};
+
+    for (let date in [...Array(grid.childNodes.length -1).keys()]) {
+        let elem = document.getElementsByClassName("date").item(date);
+        data[elem.children[0].children[0].value] = [[], [], Number(elem.children[0].children[1].children[1].value)];
+
+        for (let i in [...Array(elem.children[1].children.length - 1).keys()]) {
+            data[elem.children[0].children[0].value][0].push(elem.children[1].children[i].value);
+        }
+
+        for (let i in [...Array(elem.children[2].children.length - 1).keys()]) {
+            data[elem.children[0].children[0].value][1].push(elem.children[2].children[i].value);
+        }
+    }
+
+    let dates = generate_dates(data);
+
+    let code =  ``;
+
+    for (let date in dates) {
+        code += `<div class="output-date">
+    <h1>` + new Intl.DateTimeFormat('it-IT', {
+        dateStyle: "long",
+    }).format(new Date(date)) + `</h1>
+        <div class="name-space">`;
+
+        for (let name of dates[date]) {
+            code += "<p>" + name + "</p>";
+        }
+        
+        code += `   </div>
+</div>`;
+    }
+
+    document.getElementById("output-code").innerHTML = code;
+
+    document.getElementById("add-date").style.display = "none";
+    document.getElementById("generate").style.display = "none";
+
+    document.getElementById("output").style.display = "block"
+    document.getElementById("main").style.display = "none"
+}
---
+++ sheets.js
@@ -0,0 +1,86 @@
+const CLIENT_ID = "673269621083-gtgdpjc2nsmv71ed33m2areru5a3csjn.apps.googleusercontent.com";
+const API_KEY = "AIzaSyAH3GpeJCDZK92bUpEYYxdDIPwTDb2om0Q";
+
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
+
+const DISCOVERY_DOC = "Null";
+
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
+
+document.getElementById('authorize_button').style.visibility = 'hidden';
+document.getElementById('signout_button').style.visibility = 'hidden';
+
+/**
+ * Callback after api.js is loaded.
+ */
+function gapiLoaded() {
+  gapi.load('client', initializeGapiClient);
+}
+
+/**
+ * Callback after the API client is loaded. Loads the
+ * discovery doc to initialize the API.
+ */
+async function initializeGapiClient() {
+await gapi.client.init({
+    apiKey: API_KEY,
+    discoveryDocs: [DISCOVERY_DOC],
+});
+gapiInited = true;
+maybeEnableButtons();
+}
+
+/**
+ * Callback after Google Identity Services are loaded.
+ */
+function gisLoaded() {
+tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: CLIENT_ID,
+    scope: SCOPES,
+    callback: '', // defined later
+});
+gisInited = true;
+maybeEnableButtons();
+}
+
+function maybeEnableButtons() {
+    if (gapiInited && gisInited) {
+      document.getElementById('authorize_button').style.visibility = 'visible';
+    }
+}
+
+function handleAuthClick() {
+    tokenClient.callback = async (resp) => {
+      if (resp.error !== undefined) {
+        throw (resp);
+      }
+      document.getElementById('signout_button').style.visibility = 'visible';
+      document.getElementById('authorize_button').innerText = 'Refresh';
+      await listMajors();
+    };
+
+    if (gapi.client.getToken() === null) {
+      // Prompt the user to select a Google Account and ask for consent to share their data
+      // when establishing a new session.
+      tokenClient.requestAccessToken({prompt: 'consent'});
+    } else {
+      // Skip display of account chooser and consent dialog for an existing session.
+      tokenClient.requestAccessToken({prompt: ''});
+    }
+  }
+
+  /**
+   *  Sign out the user upon button click.
+   */
+  function handleSignoutClick() {
+    const token = gapi.client.getToken();
+    if (token !== null) {
+      google.accounts.oauth2.revoke(token.access_token);
+      gapi.client.setToken('');
+      document.getElementById('content').innerText = '';
+      document.getElementById('authorize_button').innerText = 'Authorize';
+      document.getElementById('signout_button').style.visibility = 'hidden';
+    }
+  }
